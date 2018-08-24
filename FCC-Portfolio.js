da6de8d6f22a6d7cf4761a2b80e0dfdb8075b40b
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-08-23 19:33:01 -0500

    Added JS buttons to portfolio page.

---
+++ FCC-Portfolio.js
@@ -0,0 +1,20 @@
+﻿var projects = ["https://codepen.io/mmookow/full/eKQXOg/",     "https://codepen.io/mmookow/full/zaQKjE/",
+"https://codepen.io/mmookow/full/zaXWGY/",
+"https://codepen.io/mmookow/full/XBbwwm/"];
+var count = 0;
+function next(){
+  count++;
+  var slider = document.getElementById("slider");
+  if(count>=projects.length){
+    count = 0;
+  }
+  slider.src = projects[count];
+}
+function prev(){
+  count--;
+  var slider = document.getElementById("slider");
+  if (count < 0){
+    count = projects.length-1;
+  }
+  slider.src = projects[count];
+}
